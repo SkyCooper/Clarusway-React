e262478d9a3c355009a52461ab07e9b7a5fc1c60
Cooper <dancersoftaranto@gmail.com>
2022-10-05 21:54:39 +0300

    my app first

---
+++ my-app/src/components/app.js
@@ -0,0 +1,22 @@
+import Hello from "./hello";
+import Buttons from "./buttons"
+
+const colors = {
+color 
+}
+
+const App = () => {
+return (
+  <div>
+    <h3>
+      User App from app
+    </h3>
+    <Hello/>
+    <Buttons/>
+    <Buttons/>
+    
+  </div>
+)
+}
+
+export default App
---
+++ my-app/src/components/buttons.js
@@ -0,0 +1,10 @@
+const Button = () => {
+  return(
+    <div>
+      <input type="text" placeholder="react deneme"/>
+      <button style={{backgroundColor :"blue", color : "white"}}>Gönder</button>
+    </div>
+  )
+}
+
+export default Button
