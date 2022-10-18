0aae9d2d44b75625a8ac9b04aecea3324584ca3c
Cooper <dancersoftaranto@gmail.com>
2022-10-18 11:15:14 +0300

    life csyle

--- rc08_lifecycle-useEffect/src/App.js
+++ rc08_lifecycle-useEffect/src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
+import Timer from "./components/Timer";
+import UseEffectHook from "./components/UseEffectHook";
+import User from "./components/User";
 
 
 function App() {
   const [show, setShow] = useState(true)
   return (
     <div className="container text-center mt-4">
-      <button onClick={() => setShow(!show)} className="btn btn-success">
+      {/* <button onClick={() => setShow(!show)} className="btn btn-success">
         {show ? "Show" : "Hide"}
-      </button>
-      {show && <LifeCycleMethods />}
+      </button> */}
+      {/* {show && <LifeCycleMethods />} */}
+      {/* {show && <UseEffectHook />} */}
+
+      <User/>
     </div>
   );
 }
---
+++ rc08_lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -0,0 +1,91 @@
+//?===============================================
+//?                USEEFFECT HOOK
+//?===============================================
+//! UseEffect Hook'u fonksiyonel componentler'de yan etkileri
+//! (side effect) gerceklestirmek icin kullanilir.
+//? componentDidMount,componentDidUpdate,ve componentWillUnmount
+//! metotlarinin bir birlesimi gibi dusunulebilir.
+
+import { useState, useEffect } from "react";
+
+//! useEffect(() => {
+//*   /* ComponentDidMount code */
+//! }, []);
+
+//! useEffect(() => {
+//*   */ ComponentDidMount + componentDidUpdate code */
+//! }, [var1, var2]);
+
+//! useEffect(() => {
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, []);
+
+
+//! useEffect(() => {
+//*   //* componentDidMount code + componentDidUpdate code */
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, [var1, var2]);
+
+
+const UseEffectHook = () => {
+  useEffect(() => {
+    //? snipet iskleti ve bölümlerin özellikleri;
+    //? ComponentDidMount + ComponentDidUpdate bölümü
+
+    return () => {
+      //? Clean-up function (ComponentWillUnmount) bölümü
+    };
+  }, []); //? Dependency Array bölümü
+
+  const [count, setCount] = useState(0);
+
+  //! ComponentDidMount
+  //! fetch, asyn-await, localStorage, setTimeout, setInterval(), işlemleri bu alanda yazılır.
+  // useEffect(() => {
+  //   console.log("2-Mounting");
+  //   setTimeout(() => {
+  //     alert("Data fetch");
+  //   }, 3000);
+  // }, []);
+
+  //? ComponentDidMount + ComponentDidUpdate
+  // useEffect(() => {
+  //   console.log("2-Mounting + Updating olarak çalışır.");
+  //   setTimeout(() => {
+  //     alert("Data fetch");
+  //   }, 1000);
+  // }, [count]);
+
+  //? ComponentDidount
+  const fetchFata = () => {
+    console.log("Data Fetched");
+  };
+  useEffect(() => {
+    const timerId = setInterval(fetchFata, 1000);
+    console.log("Mounting");
+
+    //? ComponentWillUnmount
+    return () => {
+      clearInterval(timerId);
+      console.log("Unmounting");
+    };
+  }, []);
+
+  console.log("1-Rendering");
+
+  return (
+    <div className="container text-center">
+      <h1 className="text-danger">USE EFFECT</h1>
+      <h3>COUNT={count}</h3>
+      <button className="btn btn-info" onClick={() => setCount(count + 1)}>
+        INC
+      </button>
+    </div>
+  );
+};
+
+export default UseEffectHook;
