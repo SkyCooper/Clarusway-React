8b956283adeb4a850f74b1d73d5e14f2d49ef8d0
Cooper <dancersoftaranto@gmail.com>
2022-10-23 13:29:32 +0300

    post-class tekrar

--- rc11_router-intro/router-intro-inClass/src/App.js
+++ rc11_router-intro/router-intro-inClass/src/App.js
@@ -6,9 +6,19 @@ import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//! REACT ROUTER;
+// react bir library(yani kütüphane), framework değil,
+// bundan dolayı içerisinde herşey hazır olarak bulunmuyor,
+// serbest olarak isteyen istediğini yükleyip/ekleyip kullanabiliyor.
+// react-router kütüphanesinin de ayrıca yüklenmesi gerekiyor.
+// yarn add react-router-dom
+// böyle yazılırsa en son versiyonu yüklenmiş olur. (şu anda 6.4.2)
+// yarn add react-router-dom@6.3 şeklinde yazılırsa istenen versiyon yüklenir.
+
+
 //? BrowserRouter
 // HTML 5 History API üzerine kurulmuştur. Sayfa yönlendirmelerini tutan ana bileşendir. Route yapısı kullanılacak tüm bileşenler / sayfalar BrowserRouter arasında olmak zorundadır.
-// Tanımlanan Yerlere Sayfaların Render Edileceğini Bildiren component. En dış sarmallayıcıdır
+// Tanımlanan Yerlere Sayfaların Render Edileceğini Bildiren component. En dış sarmallayıcıdır. {BrowserRouter as Router} olarak kullanılabilir. App/Index js nin birisinde sarmalayıcı olarak kullanılabilir.
 
 // ?Routes:
 // Konum her değiştiğinde, Routes en iyi eşleşmeyi bulmak için childları olan tüm alt Route öğelerine bakar ve kullanıcı arabiriminin bu dalını oluşturur.
@@ -22,22 +32,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Nav-Footer ==> amca gibi kodlanabilir.
 
 //? SSR - CSR
-// react-rooter uygulamalarında server-side-routing (SSR) değil client-side-routing (CSR)  yapılıyor. yani ilk seferde bütün bilgiler kullanıcıya ham dosyalar olarak geliyor ve onun bilgisayarında parse ediliyor. Her defasında refresh olmuyor. Bir kere yüklendikten sonra hızlıca sayfa geçişi yapılabiliyor. CSR CEO açısında uygun değil, ayrıca harici kütüphaneler kullanmak gerekiyor.
+// react-rooter uygulamalarında server-side-routing (SSR) değil client-side-routing (CSR)  yapılıyor. yani ilk seferde bütün bilgiler kullanıcıya ham dosyalar olarak geliyor ve onun bilgisayarında parse ediliyor. Her defasında refresh olmuyor. Bir kere yüklendikten sonra hızlıca sayfa geçişi yapılabiliyor. CSR SEO açısında uygun değil, arama motorlarının işini zorlaştırıyor.  ayrıca harici kütüphaneler kullanmak gerekiyor(react-helmet gibi). CSR server üzerinden yükü alır, fakat loading-time biraz uzun olabilir.(Lazy-Loading kavramı burada devreye giriyor.)
 
 // Single page application yani kısa adıyla SPA, tek HTML sayfası yükleyen bir uygulamadır ve uygulamanın çalışması için gerekli tüm dosyaları (JavaScript, CSS vb) içerir. Sayfa veya sonraki sayfalarla olan herhangi bir etkileşim için servera gidip gelmesi gerektirmez; bu da sayfanın yeniden yüklenmediği anlamına gelir.
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter> {/* 1 */}
         <Nav />
-        <Routes>
+        {/* her sayfada olcaksa BrowserRouter içinde fakat Routes dışında yazılır. */}
+        <Routes>  {/* 2 */}
+        {/* Routes v5 ten sonra çıktı, önceden Switch vardı */}
+
           <Route path="/" element={<Home />} />
-          <Route path="/istructors" element={<Instructors />} />
+          <Route path="/instructors" element={<Instructors />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
+        {/* olmayan bir link gelirse not-found compenenti gelsin */}
+
         </Routes>
         <Footer />
+      {/* her sayfada olcaksa BrowserRouter içinde fakat Routes dışında yazılır. */}
       </BrowserRouter>
     </>
   );
--- rc11_router-intro/router-intro-inClass/src/components/About.jsx
+++ rc11_router-intro/router-intro-inClass/src/components/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-  return <>About</>;
+  return <div>About</div>;
 };
 
 export default About;
