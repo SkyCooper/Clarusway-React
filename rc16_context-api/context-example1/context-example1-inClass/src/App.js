92ef45e4dd374c9f9a4af40175cc114fec4e147b
Cooper <dancersoftaranto@gmail.com>
2022-11-02 22:29:24 +0300

    START

--- rc16_context-api/context-example1/context-example1-inClass/src/App.js
+++ rc16_context-api/context-example1/context-example1-inClass/src/App.js
@@ -6,23 +6,27 @@ import People from "./pages/People";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PersonDetail from "./pages/PersonDetail";
 import Login from "./pages/Login";
+import { LoginContext } from "./context/LoginContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navs />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
+    //! 2-PROVİDNG
+    <LoginContext.Provider>
+      <BrowserRouter>
+        <Navs />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
 
-        <Route path="people" element={<People />} />
-        <Route path="people/:id" element={<PersonDetail />} />
+          <Route path="people" element={<People />} />
+          <Route path="people/:id" element={<PersonDetail />} />
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </LoginContext.Provider>
   );
 }
 
---
+++ rc16_context-api/context-example1/context-example1-inClass/src/context/LoginContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+//! 1-CREATE
+//? parantez içine initial/dfault valu vermek zorunlu değil, boş bırakılabilir.
+export const LoginContext = createContext()
