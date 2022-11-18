036ac1e7d5d8be5f726d3d1d196bfe2b6a21af05
Cooper <dancersoftaranto@gmail.com>
2022-11-18 13:40:22 +0300

    createAsyncThunk

--- rc22_redux-project/redux-project-inClass/src/features/newsSlice.jsx
+++ rc22_redux-project/redux-project-inClass/src/features/newsSlice.jsx
@@ -39,11 +39,14 @@ export const getNews = createAsyncThunk(
 
   async (thunkAPI, { rejectWithValue }) => {
     //! asyn callback function
-    const API_KEY = "02d142c50d8b4247b974b2532343517";
-    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+    const API_KEY = "2ae73e0e340345b39a263c6557eb8796";
+    const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
     try {
       const { data } = await axios(url);
       return data.articles;
+      //? yani API işlemi başarılı oldu ve data içinden article alındı,
+      //? buradan gelen sonuç builder.fulfilled içindeki payload aracılığı ile
+      //? initialState olarak boş tanımlanan newsList'e aktarılıyor.
     } catch (error) {
       console.log(error);
       return rejectWithValue("Something went wrong");
@@ -59,15 +62,20 @@ const newsSlice = createSlice({
       state.newsList = [];
     },
   },
+  //? extraReducers içinde side-effectlerin stateleri kontrol ediliyor.
   extraReducers: (builder) => {
     builder
+      //? builder içinde bunlar otomatik ön tanımlı;
+      //? pending yani API isteği başladı
       .addCase(getNews.pending, (state) => {
         state.loading = true;
       })
+      //? fulfilled yani API isteği başarılı olarak sonuçlandı
       .addCase(getNews.fulfilled, (state, { payload }) => {
         state.newsList = payload;
         state.loading = false;
       })
+      //? rejected yani API isteği BAŞARISIZ olarak sonuçlandı
       .addCase(getNews.rejected, (state, { payload }) => {
         state.loading = false;
         state.error = payload;
