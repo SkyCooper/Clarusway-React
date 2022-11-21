be8f5b6e48b3a7bc493c5f0fd63d02a83cc524b0
Cooper <dancersoftaranto@gmail.com>
2022-11-21 23:05:09 +0300

    store

--- rc25_stock-app/stock-app-starter/src/app/store.jsx
+++ rc25_stock-app/stock-app-starter/src/app/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage/"; // defaults to localStorage for web
 //* import storage from 'redux-persist/lib/storage/session'
@@ -25,6 +26,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock : stockReducer,
   },
   //todo, serializer hatası için middleware ekleniyor.
   middleware: (getDefaultMiddleware) =>
