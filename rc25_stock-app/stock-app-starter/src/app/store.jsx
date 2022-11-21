1407359c8bcb1221c171cfc5ac480de2a013a805
Cooper <dancersoftaranto@gmail.com>
2022-11-21 22:39:30 +0300

    persister

--- rc25_stock-app/stock-app-starter/src/app/store.jsx
+++ rc25_stock-app/stock-app-starter/src/app/store.jsx
@@ -1,10 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/"; // defaults to localStorage for web
+//* import storage from 'redux-persist/lib/storage/session'
+//* default olarak local gelir, onu session olarak değiştirebiliriz.
+//? redux-persist sayfa refresh olduğunda state'lerin kaybolmamasını ve logout olmamayı sağlıyor.
+
+//todo, serializer hatası varsa consolda bunu gidermek için bunları al kullan olarak import ediyoruz.
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
+  //todo, serializer hatası için middleware ekleniyor.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
 export default store;
