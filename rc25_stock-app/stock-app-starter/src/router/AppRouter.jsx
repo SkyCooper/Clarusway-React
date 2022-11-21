607ff78f288107825929a83026a62ec380c0f638
Cooper <dancersoftaranto@gmail.com>
2022-11-21 22:02:21 +0300

    routes

--- rc25_stock-app/stock-app-starter/src/router/AppRouter.jsx
+++ rc25_stock-app/stock-app-starter/src/router/AppRouter.jsx
@@ -4,6 +4,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
+import Firms from "../pages/Firms";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
 
 const AppRouter = () => {
   return (
@@ -12,7 +18,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="purchases" element={<Purchases />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="firms" element={<Firms />} />
+            <Route path="brands" element={<Brands />} />
+            <Route path="products" element={<Products />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
