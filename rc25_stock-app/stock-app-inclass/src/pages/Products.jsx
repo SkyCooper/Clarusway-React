3a051bb453cd5eb1f9279b7cfed8c5e4963da639
Cooper <dancersoftaranto@gmail.com>
2022-11-24 17:41:54 +0300

    sort products

--- rc25_stock-app/stock-app-inclass/src/pages/Products.jsx
+++ rc25_stock-app/stock-app-inclass/src/pages/Products.jsx
@@ -16,8 +16,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { btnHoverStyle, arrowStyle } from "../styles/globalStyle";
 import UpgradeIcon from "@mui/icons-material/Upgrade";
 import VerticalAlignBottomIcon from "@mui/icons-material/VerticalAlignBottom";
-import ProductModal from "../components/modals/ProductModal"
-
+import ProductModal from "../components/modals/ProductModal";
 
 //todo, Firms componentini kopyalaıp üzerinden değiştirdik...
 const Products = () => {
@@ -25,11 +24,14 @@ const Products = () => {
   const { products } = useSelector((state) => state.stock);
   const [open, setOpen] = useState(false);
   const [info, setInfo] = useState({});
+  //! Siralama da kullanilacak toggle state'i
   const [toggle, setToggle] = useState({
-    brand: false,
-    name: false,
+    name: 1,
+    brand: 1,
     stock: 1,
   });
+  //! Siralanacak local state (sutun verilerinin local state hali)
+  const [sortedProducts, setSortedProducts] = useState(products);
 
   useEffect(() => {
     getBrands();
@@ -37,9 +39,26 @@ const Products = () => {
     getProducts();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleSortNumber = (arg) => {
+  const handleSort = (arg, type) => {
     setToggle({ ...toggle, [arg]: toggle[arg] * -1 });
     //? yani toogle objesini aç, başlangıçta 1 olan ve argüman olarak gelen stock değeri  -1 olacak, veya -1 olan +1 olacak, devamlı terslenecek..
+    setSortedProducts(
+      sortedProducts
+        ?.map((item) => item)
+        .sort((a, b) => {
+          if (type === "date") {
+            return toggle[arg] * (new Date(a[arg]) - new Date(b[arg]));
+          } else if (type === "number") {
+            return toggle[arg] * (a[arg] - b[arg]);
+          } else {
+            if (toggle[arg] === 1) {
+              return b[arg] > a[arg] ? 1 : b[arg] < a[arg] ? -1 : 0;
+            } else {
+              return a[arg] > b[arg] ? 1 : a[arg] < b[arg] ? -1 : 0;
+            }
+          }
+        })
+    );
   };
   return (
     <Box>
@@ -50,8 +69,13 @@ const Products = () => {
       <Button variant="contained" color="error" onClick={() => setOpen(true)}>
         New Product
       </Button>
-      
-      <ProductModal open={open} setOpen={setOpen} info={info} setInfo={setInfo} />
+
+      <ProductModal
+        open={open}
+        setOpen={setOpen}
+        info={info}
+        setInfo={setInfo}
+      />
 
       {products?.length >= 0 && (
         <TableContainer component={Paper} sx={{ mt: 3 }} elevation={10}>
@@ -60,7 +84,40 @@ const Products = () => {
               <TableRow>
                 <TableCell align="center">#</TableCell>
                 <TableCell align="center">Category</TableCell>
-                <TableCell align="center">
+
+                {sortedProducts?.length > 0 && (
+                  <>
+                    <TableCell align="center">
+                      <Box sx={arrowStyle}>
+                        <div>Brand</div>
+                        {toggle.name === 1 && <UpgradeIcon />}
+                        {toggle.name !== 1 && <VerticalAlignBottomIcon />}
+                      </Box>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Box
+                        sx={arrowStyle}
+                        onClick={() => handleSort("name", "text")}
+                      >
+                        <div>Name</div>
+                        {toggle.name === 1 && <UpgradeIcon />}
+                        {toggle.name !== 1 && <VerticalAlignBottomIcon />}
+                      </Box>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Box
+                        sx={arrowStyle}
+                        onClick={() => handleSort("stock", "number")}
+                      >
+                        <div>Stock</div>
+                        {toggle.stock === 1 && <UpgradeIcon />}
+                        {toggle.stock !== 1 && <VerticalAlignBottomIcon />}
+                      </Box>
+                    </TableCell>
+                  </>
+                )}
+
+                {/* <TableCell align="center">
                   <Box sx={arrowStyle}>
                     <div>Brand</div>
                     {true && <UpgradeIcon />}
@@ -75,15 +132,13 @@ const Products = () => {
                   </Box>
                 </TableCell>
                 <TableCell align="center">
-                  <Box
-                    sx={arrowStyle}
-                    onClick={() => handleSortNumber("stock")}
-                  >
+                  <Box sx={arrowStyle} onClick={() => handleSort("stock")}>
                     <div>Stock</div>
                     {toggle.stock === 1 && <UpgradeIcon />}
                     {toggle.stock !== 1 && <VerticalAlignBottomIcon />}
                   </Box>
-                </TableCell>
+                </TableCell> */}
+                
                 <TableCell align="center">Operation</TableCell>
               </TableRow>
             </TableHead>
