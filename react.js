3666f11b3fbb1ed01fbe554ae0d7f34092a3d30c
Cooper <dancersoftaranto@gmail.com>
2023-02-27 17:19:46 +0300

    react nedir

---
+++ react.js
@@ -0,0 +1,28 @@
+//? react nedir?
+// 2011 yılında facebook tarafından geliştirilen component based, kullanıcı arayüzü oluşturan  hızlı bir javascript (library) kütüphanesidir. En popüler js kütüphanesidir. Amacı dinamik web sayfalarını daha hızlı görüntüleyebilmektir. Virtual DOM özelliği sayesinde daha az render ile daha hızlı çalışır.
+
+// react-native (ios-android) öğrenmek için biraz çalışma yapıp sonra web / app dönüşümü yapılabilir.
+
+//* library - framework
+// library, önden yazılmıl kodları vardır, istediğimizi alıp kullanabiliriz. 
+// framework, herşey kendi içinde var ve bunun dışına çıkılmaz(ekosistem)-öğrenmesi daha zor,zorunlulukları var.
+
+//* component
+// kendi HTML - CSS ve JS'si olan birimdir. lego mantığı , parçalar her yerde kullanılabilir. bu sayede daha modülerdir ve hızlı çalışır.
+
+// lazy loading : sayfanın tamamının değil sadece görünen kısmının render olması/yüklenmesi demek
+
+// react modern JS kullanıyor, eski browser'larda BABEL isimli transpilier vasıtasıyla çeviriyor.
+
+// MVC (mimarisi) : Model View Controller  demektir. React view (UI) katmanında (layer) çalışır, model ve controller backend katmanıdır. Değişiklik olduğu zaman sadece o katmanda işlem yapılabilir, mesela react yerine angular kullanılmak isteyince sadece view katmanı değişir.
+
+//* react-developer-tool 
+// https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi
+// eğer bir web sitesinde react kullanılmışsa simge renkli onblur. 
+
+// https://npmtrends.com/angular-vs-react-vs-vue
+// hangi framework ne kadar indirilmiş böyle bakılabilir.
+
+//* JSX - Javascript Xml
+// HTML ve JavaScript kodlarının aynı dosyada kullanılabilmesi
+// function veya class based component kullanıldığında retun bloğu ve içinde jsx kullanılır, diğer alanlarda düz js kullanılır.
