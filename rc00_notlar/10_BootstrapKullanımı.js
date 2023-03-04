0e912a296f1878a901aefe743a1fab92534b97ef
Cooper <dancersoftaranto@gmail.com>
2023-03-04 15:39:35 +0300

    bootstrap

--- rc00_notlar/10_BootstrapKullanımı.js
+++ rc00_notlar/10_BootstrapKullanımı.js
@@ -2,10 +2,22 @@
 //* https://react-bootstrap.github.io/getting-started/introduction
 
 //! 1- ilkel kullanım, yani Html'deki gibi kullanım, BOOTSTRAP İÇİN;
+//todo, birinci yöntem
 yarn add bootstrap 
 
 // css linkini App.js veya index.js içine yapıştır.
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+//todo, ikinci yöntem yöntem
+// 1- public içindeki index.html head tagı içerisine ekle;
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link>
+
+
+// 2- public içindeki index.html body kapanış tagı üstüne ekle;
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
 
 //? Artık kullanıma hazır, bootstrap classlarını clasName="" ile kullanabiliriz.
 // böylece daha önceden klasik html ile hazırlanmış ve bootstrap kullanılmış projeleri
