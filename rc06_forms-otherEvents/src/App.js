734e088951c0b2566d444108e070c9354039395a
Cooper <dancersoftaranto@gmail.com>
2023-03-04 17:58:09 +0300

    name change

--- rc06_forms-otherEvents/src/App.js
+++ rc06_forms-otherEvents/src/App.js
@@ -1,14 +1,14 @@
-// import Form from "./components/1-forms/Form";
-// import Formobject from "./components/1-forms/Formobject";
-// import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
+import Form from "./components/1-forms/Form";
+import Formobject from "./components/1-forms/Formobject";
+import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
 import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
-      {/* <Form /> */}
-      {/* <Formobject /> */}
-      {/* <KeyboardEvents /> */}
+      <Form />
+      <Formobject />
+      <KeyboardEvents />
       <MouseEvent />
     </div>
   );
--- rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -25,40 +25,40 @@ const MouseEvent = () => {
   };
 
   return (
-    <div className="container text-center d-flex flex-column align-items-center mt-4">
-      <h2 className="text-danger">MOUSE EVENTS</h2>
+      <div className="container text-center d-flex flex-column align-items-center mt-4">
+          <h2 className="text-danger">MOUSE EVENTS</h2>
 
-      <div
-        id="todo-1"
-        className="bg-success text-light w-50 p-4"
-        onMouseEnter={() => setShowHint(true)}
-        onMouseOut={() => setShowHint(false)}
-      >
-        todo item 1
+          <div
+              id="todo-1"
+              className="bg-success text-light w-50 p-4"
+              onMouseEnter={() => setShowHint(true)}
+              onMouseOut={() => setShowHint(false)}
+          >
+              todo item 1 - onMouseEnter/onMouseOut
+          </div>
+          {showHint && <span>hello React</span>}
+
+          <div
+              id="todo-2"
+              className="bg-success text-light w-50 p-4 mt-4"
+              onDoubleClick={handleDoubleClick}
+          >
+              todo item 2 - onDoubleClick
+          </div>
+
+          <div
+              id="todo-3"
+              className="bg-success text-light w-50 p-4 my-4"
+              onMouseMove={handleMouseMove}
+          >
+              todo item 3 - onMouseMove
+          </div>
+
+          <p>X and Y</p>
+          <p className="text-danger fw-bold">
+              {coordX} {coordY}
+          </p>
       </div>
-      {showHint && <span>hello React</span>}
-
-      <div
-        id="todo-2"
-        className="bg-success text-light w-50 p-4 mt-4"
-        onDoubleClick={handleDoubleClick}
-      >
-        todo item 2
-      </div>
-
-      <div
-        id="todo-3"
-        className="bg-success text-light w-50 p-4 my-4"
-        onMouseMove={handleMouseMove}
-      >
-        todo item 3
-      </div>
-
-      <p>X and Y</p>
-      <p className="text-danger fw-bold">
-        {coordX} {coordY}
-      </p>
-    </div>
   );
 };
 
