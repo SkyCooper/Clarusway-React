37ba35da985069e1cb585757a22f5e96ac6643db
Cooper <dancersoftaranto@gmail.com>
2023-03-12 15:51:10 +0300

    routerv1

--- rc00_notlar/19_Routerİşlemleri.js
+++ rc00_notlar/19_Routerİşlemleri.js
@@ -2,7 +2,9 @@
 // react bir library(yani kütüphane), framework değil,
 // bundan dolayı içerisinde herşey hazır olarak bulunmuyor,
 // serbest olarak isteyen istediğini yükleyip/ekleyip kullanabiliyor.
-react-router kütüphanesinin de ayrıca yüklenmesi gerekiyor.
+
+// react-router kütüphanesinin de ayrıca yüklenmesi gerekiyor.
 yarn add react-router-dom
+
 böyle yazılırsa en son versiyonu yüklenmiş olur. (şu anda 6.4.2)
 yarn add react-router-dom@6.3 şeklinde yazılırsa istenen versiyon yüklenir.
